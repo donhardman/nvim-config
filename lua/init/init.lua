3095dcb29ac1478fb482ec7bc516ada1bacd276f
Don Hardman <don.hardman@muvon.io>
2024-04-25 23:23:37 +0700

    Use defined Git Gutter colors for status line diff renering

--- lua/init/init.lua
+++ lua/init/init.lua
@@ -23,7 +23,21 @@ require('lualine').setup({
     lualine_a = {'mode'},
     lualine_b = {
 			'branch',
-			'diff',
+			{
+				'diff',
+				colored = true, -- Displays a colored diff status if set to true
+				diff_color = {
+					-- Same color values as the general color option can be used here.
+					added    = 'GitGutterAdd',    -- Changes the diff's added color
+					modified = 'GitGutterChange', -- Changes the diff's modified color
+					removed  = 'GitGutterDelete', -- Changes the diff's removed color you
+				},
+				symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
+				source = nil, -- A function that works as a data source for diff.
+				-- It must return a table as such:
+				--   { added = add_count, modified = modified_count, removed = removed_count }
+				-- or nil on failure. count <= 0 won't be displayed.
+			}
 		},
 		lualine_c = {'filename', 'searchcount'},
     lualine_x = {
